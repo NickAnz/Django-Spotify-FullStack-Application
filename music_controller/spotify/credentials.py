ee20a4a5b89eec37f33c8272fd21d8e01e48a4bc
Nicholas A <48538292+NickAnz@users.noreply.github.com>
2021-02-12 10:40:49 +1100

    Update credentials.py

--- music_controller/spotify/credentials.py
+++ music_controller/spotify/credentials.py
@@ -1,4 +1,5 @@
 CLIENT_ID = "e121dc7b718840fb8c407a0685b63b43"
 CLIENT_SECRET = "e6c0f8d6b90f4285b2b1fbe31a08b90d"
 # this will redirect to the spotify callback view which will then spit us back onto the homepage
+# Spotify API disabled due to public placement on GitHub 
 REDIRECT_URI = "http://127.0.0.1:8000/spotify/redirect"
